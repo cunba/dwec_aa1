92b5d3ae06753243e6cfe6d4669fa502133f5d84
cunba <ire.cunba@gmail.com>
2025-08-08 01:31:35 +0200

    mark selected category

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,28 @@
+let currentCategory
+
 let drawData = (data) => {
-    data.forEach(category => {
+    data.forEach((category, index) => {
         let parent = document.getElementsByTagName('ul')[0]
         let child = document.createElement('li')
         // child.innerText = JSON.stringify(category)
         child.innerText = category.name
+        child.id = category.name
+        child.onclick = onCategoryClicked
+        if (index === 0) {
+            child.className = 'selected'
+            currentCategory = child
+        }
         parent.appendChild(child)
     })
+}
+
+let onCategoryClicked = (event) => {
+    console.log(currentCategory)
+    const category = document.getElementById(event.target.id)
+    category.className = 'selected'
+
+    currentCategory.className = 'not-selected'
+    currentCategory = category
 }
 
 fetch("http://localhost:3000/categories")
